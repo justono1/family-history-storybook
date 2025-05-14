5b0284b501426ede5da4bc9cbc386488d8643f60
Justin Gauthier <itsme@justingauthier.com>
2025-05-13 23:16:15 -0400

    general page stubbed in

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,29 +6,35 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <nav className={styles.nav}>LOGO</nav>
+      <nav className={styles.nav}>
+        <span className="h2">Family Storybooks</span>
+        <a
+          href="https://github.com/justono1/family-history-storybook"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={"32"} />
+        </a>
+      </nav>
       <main className={styles.main}>
         {/* ← Left sidebar */}
-        <aside className={styles.sidebar}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero nemo
-          laudantium beatae officiis sed corporis, placeat sequi aliquid quod
-          repellat ratione commodi ab repellendus, iste ex accusamus! Animi,
-          enim quasi.
-          <footer className={styles.footer}>
-            <a
-              href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithub size={"32"} />
-            </a>
-          </footer>
-        </aside>
+        <aside className={styles.sidebar}>Lorem ipsum dolor,</aside>
 
         {/* → Right content area */}
         <section className={styles.content}>
           <h1>Welcome!</h1>
           <p>This is your main content area.</p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero nemo
+          laudantium beatae officiis sed corporis, placeat sequi aliquid quod
+          repellat ratione commodi ab repellendus, iste ex accusamus! Animi,
+          enim quasi. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Vero nemo laudantium beatae officiis sed corporis, placeat sequi
+          aliquid quod repellat ratione commodi ab repellendus, iste ex
+          accusamus! Animi, enim quasi. Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Vero nemo laudantium beatae officiis sed corporis,
+          placeat sequi aliquid quod repellat ratione commodi ab repellendus,
+          iste ex accusamus! Animi, enim quasi. Lorem ipsum dolor, sit amet
+          consectetur adipisicing elit.
         </section>
       </main>
     </div>
